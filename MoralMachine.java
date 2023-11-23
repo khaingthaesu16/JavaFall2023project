30329a308d746f7e9042c047cc41a171e0e3c324
Khaing T Su <122903417+khaingthaesu16@users.noreply.github.com>
2023-11-23 18:46:25 -0500

    Update MoralMachine.java

--- MoralMachine.java
+++ MoralMachine.java
@@ -26,6 +26,7 @@ public class MoralMachine
         return decision;
     }
 
+   //Scenrio 1 = Saving more lives depending on legal rules
     public static boolean savingMoreLives(int scenario, String g1, String g2)
     {
         boolean result = false;
@@ -33,20 +34,21 @@ public class MoralMachine
         if (scenario < 11)
         //no legal complications
         {
-            
+            result = groupSize( g1,  g2);
         }
         else if (scenario < 22)
         {
-            
+            result = groupSize(g1, g2);
         }
         else
         {
-            
+           result = true;
         }
         
         return result;
     }
     
+ //for every scenerio
     public static boolean groupSize(String g1, String g2)
     {
       if(g1.length() > g2.length())
@@ -63,5 +65,45 @@ public class MoralMachine
       }
             
    }
+
+
+//Scenerio 2 = Saving more humans not depending on the legal rules
+   public static boolean savingmoreHuman(String g1, String g2)
+   {
+      boolean result = false;
+      
+      int g1Animalcount = 0;
+      int g2Animalcount = 0;
+      for(int i= 0; i<g1.length(); i++)
+      {
+         char g1Animal = g1.charAt(i);
+         if( (g1Animal == 's' ) || (g1Animal == 't'))
+         g1Animalcount ++;
+          
+       }
+    
+      for (int j = 0;j<g2.length(); j++)
+      {
+         char g2Animal = g2.charAt(j);
+         if ((g2Animal =='s' )|| (g2Animal =='t'))
+         g2Animalcount ++;
+      }
+
+      if (g1Animalcount>g2Animalcount)
+      {
+         result = false;
+       }
+       else if(g1Animalcount<g2Animalcount)
+       {
+         result = true;
+        }
+        else
+        {
+         result = groupSize(g1, g2);
+        }
+        return result;
+     }
+        
+
 
 }//end class MoralMachine
